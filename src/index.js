0e78422caedc1577f22ff9f4f170b6f2f5028fdd
VasiliyRepin <36683162+VasiliyRepin@users.noreply.github.com>
2018-03-01 10:25:31 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
-module.exports = function check(str, bracketsConfig) {
+module.exports = function check(str, bracketsConfig) 
+{
   // your solution
+    while (str.length > 0)
+    {
+        temp_str = str;
+
+            for (var i =0; i < bracketsConfig.length;  i++)
+            {
+                rpl = bracketsConfig[i].join('');
+                str = str.replace(rpl, '');
+            }
+
+
+            if (str == temp_str)
+            {
+                return false;
+            }
+    }
+
+    return true;
+
 }
